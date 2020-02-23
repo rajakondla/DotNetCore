68991b995c9dd092ee619bc77e0410a84e318c79
Raja Kondla <rajakondla@gmail.com>
2020-02-23 14:36:34 +0000

    commit -m "added a comment"

--- MyPracticeWebSite/Startup.cs
+++ MyPracticeWebSite/Startup.cs
@@ -28,6 +28,9 @@ namespace MyPracticeWebSite
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
+
+        // added a cmment 
         public void ConfigureServices(IServiceCollection services)
         {
             // used for registering all necessary mvc services. Eg. In order to use HttpContext obj in controller.
